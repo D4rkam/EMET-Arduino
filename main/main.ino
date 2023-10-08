e84ee28706f72ce953fca38f45d1f2f850edda2a
ElpibeInformatico <thomaslinares32@gmail.com>
2023-10-08 12:55:46 -0300

    Nuevas Funcionalidades

--- main/main.ino
+++ main/main.ino
@@ -1,42 +1,44 @@
                 // ------ IMPORTACIONES ------ //
-#include<LiquidCrystal_I2C.h>
-#include<DHT.h>
+#include <LiquidCrystal_I2C.h>
+#include <ArduinoJson.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include <NewPing.h>
+#include <DHT.h>
 
                 // ------ CONSTANTES/INSTANCIAS ------ //
 const int DHTPin = 5;
-const int BTNPin = 2;
 const int EchoPin = 3;
 const int TriggerPin = 4;
+const int ButtonPin = 9;
+
+const int ALTURA_SENSOR_AGUA = 14;
+const double PRESION_NIVEL_MAR = 1013.25;
 
 #define DHTTYPE DHT22
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+NewPing sonar(TriggerPin, EchoPin, 200);
+SFE_BMP180 bmp180;
 DHT dht(DHTPin, DHTTYPE);
-
 
                 // ------ DEFINICION ------ //
 void setup() {
-  pinMode(BTNPin, INPUT_PULLUP);
+  Serial.begin(9600);
   
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT);
   digitalWrite(TriggerPin, LOW);
 
-  lcd.init();
-  lcd.backlight();
+  pinMode(ButtonPin, INPUT_PULLUP);
 
   dht.begin();
 
-  Serial.begin(9600);
+  bmp180.begin();  
 }
 
                 // ---------- VARIABLES ---------- //
 
-//Display LCD
-int displayMode = 0;
-int btnState = HIGH;
-int lastBtnState = HIGH;
-
-//Sensor DHT22
+//Sensor Temperatura/Humedad (DHT22)
 float humidity;
 float temp;
 
@@ -44,11 +46,31 @@ float temp;
 float distanceCm;
 float highSensor;
 
+//Sensor Presion Atmosferica (BMP180) 
+double presionAtmos;
+double altitude;
+
+//Json
+StaticJsonDocument<200> jsonDocument;
+String jsonString;
+
                 // ------ MAIN ------ //
 void loop() {
-  readDHT22(humidity, temp);
-  getRainedWater(TriggerPin, EchoPin, distanceCm, highSensor);
-  showDisplayLcd(displayMode, btnState, lastBtnState, lcd);
+  bool valueButton = digitalRead(ButtonPin);
+
+  if(valueButton == HIGH){
+    
+    readDHT22(humidity, temp);
+    getRainedWater(distanceCm);
+    getAltitudeAndPresionAtmos(presionAtmos, altitude);
+    createJson(jsonDocument, jsonString, humidity, temp, presionAtmos, distanceCm);
+    showDisplayLcd(lcd, distanceCm, presionAtmos);
+    
+    valueButton = false;
+    delay(5000);
+    lcd.clear();
+  }
+  lcd.noBacklight();
 }
 
                 // ------ FUNCIONES PERSONALIZADAS ------ //
@@ -57,6 +79,7 @@ void loop() {
 void readDHT22(float& humidity, float& temp){
   humidity = dht.readHumidity();
   temp = dht.readTemperature();
+
   if (isnan(humidity) || isnan(temp)) {
     humidity = -1;
     temp = -1;
@@ -64,52 +87,79 @@ void readDHT22(float& humidity, float& temp){
 }
 
 //Calcula la cantidad de agua llovida. Esto se logra utilizando el sensor HC-SR04
-void getRainedWater(int TriggerPin, int EchoPin, float& distanceCm, float& highSensor) {
-  long time;
-  digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
-  delayMicroseconds(10);
-  digitalWrite(TriggerPin, LOW);
+void getRainedWater(float& distanceCm) {
+
+  int dist = sonar.ping_cm();
+  distanceCm = ALTURA_SENSOR_AGUA - dist;
   
-  time = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+}
+//Obtiene la Presion Atmosferica y calcula la altitud
+void getAltitudeAndPresionAtmos(double& presion, double& altitude){
+
+  char status;
+  double T;
   
-  distanceCm = (time * 0.000001 / 34300.0 / 2.0) - highSensor;   //convertimos a distancia, en cm
+  status = bmp180.startTemperature(); //Inicio de lectura de temperatura
+  if (status != 0)
+  {   
+    delay(status); //Pausa para que finalice la lectura
+    status = bmp180.getTemperature(T); //Obtener la temperatura
+    if (status != 0)
+    {
+      status = bmp180.startPressure(3); //Inicio lectura de presión
+      if (status != 0)
+      {        
+        delay(status); //Pausa para que finalice la lectura        
+        status = bmp180.getPressure(presion, T); //Obtener la presión
+        if (status != 0)
+        {                  
+          altitude = bmp180.altitude(presion, PRESION_NIVEL_MAR); //Calcular altura    
+        }      
+      }      
+    }   
+  } 
 }
 
 //Muestra la informacion procesada en el display LCD
-void showDisplayLcd(int& displayMode, int& btnState, int& lastBtnState, LiquidCrystal_I2C lcd){
-  btnState = digitalRead(BTNPin);
-  if (btnState == LOW && lastBtnState == HIGH){
-    displayMode = (displayMode + 1) % 3;
-    
-    lcd.clear();
-    switch (displayMode){
+void showDisplayLcd(LiquidCrystal_I2C lcd, float distanceCm, double presion){
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  
+  // ------ TEMPERATURA - HUMEDAD ------ //
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print("C");
 
-      // ------ TEMPERATURA - HUMEDAD ------ //
-      case 0:
-        lcd.setCursor(0, 0);
-        lcd.print("Temp: ");
-        lcd.print(temp);
-        lcd.print("C");
-        lcd.setCursor(0, 1);
-        lcd.print("Hum: ");
-        lcd.print(humidity);
-        lcd.print("%");
-        break;
-      
-      // ------ ¿PLUVIOMETRO? ------ //
-      case 1:
-        lcd.setCursor(0, 0);
-        lcd.print("Distan: ");
-        lcd.print(distanceCm);
-        lcd.print(" cm");
-        lcd.setCursor(0, 1);
-        lcd.print("Altura: ");
-        lcd.print(highSensor);
-        lcd.print(" cm");
-        break;
-    }
-  }
-  Serial.print("Display Mode: ");
-  Serial.println(displayMode);
-  lastBtnState = btnState;
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  lcd.print(humidity);
+  lcd.print("%");
+
+  lcd.setCursor(0, 2);
+  lcd.print("Presion: ");
+  lcd.print(presion);
+  lcd.print(" Mbar");
+
+  lcd.setCursor(0, 3);
+  lcd.print("Agua: ");
+  lcd.print(distanceCm);
+  lcd.print(" cm");
+}
+
+void createJson(StaticJsonDocument<200>& jsonDocument, String& jsonString, float hum, float temp, double pre_atmos, float rained_water){
+  
+  jsonDocument["temperature"] = temp;
+  jsonDocument["humidity"] = hum;
+  jsonDocument["pre_atmos"] = pre_atmos;
+  jsonDocument["water_rained"] = rained_water;
+
+  serializeJson(jsonDocument, jsonString);
+}
+
+void sendData(){
+  /*
+  En esta funcion se utilizaria el ESP8266 para enviar los datos al servidor
+  */
 }
